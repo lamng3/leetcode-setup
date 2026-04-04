253c84d33ee813e1fd33473366fee1cb267c13c2
ltn18 <nguyentamhoanglam@gmail.com>
2026-04-04 16:13:04 -0700

    add solved problems
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

---
+++ solve/930.cpp
@@ -0,0 +1,79 @@
+// g++ -std=c++17 -DLOCAL template.cpp -o solution
+#include <bits/stdc++.h>
+using namespace std;
+
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << "[";
+    for (int i = 0; i < v.size(); i++) {
+        os << v[i] << (i == v.size() - 1 ? "" : ", ");
+    }
+    return os << "]";
+}
+
+using ll = long long;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using pii = pair<int, int>;
+
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFOR(i, n) for (int i = (n) - 1; i >= 0; i--)
+
+#define fi first
+#define se second
+#define pb push_back
+
+const int INF = 1e9;
+const ll LLINF = 2e18;
+
+const int MOD = 1e9+7;
+const int MOD_NTT = 998244353; // number theoretic transform (NTT)
+
+// 930. Binary Subarrays With Sum [Medium]
+class Solution {
+public:
+    int count(const vi& nums, const int& goal) {
+        if (goal < 0) return 0;
+        int n = (int)nums.size();
+        int res = 0, run = 0;
+        int L = 0;
+        for (int R = 0; R < n; R++) {
+            run += nums[R];
+            while (run > goal) {
+                run -= nums[L];
+                L++;
+            }
+            res += R - L + 1;
+        }
+        return res;
+    }
+
+    int numSubarraysWithSum(vi& nums, int goal) {
+        return count(nums, goal) - count(nums, goal-1);
+    }
+};
+
+#if defined(LOCAL) || defined(ONLINE_JUDGE)
+void preprocess() {
+    
+}
+
+// cout << Solution().solve() << '\n';
+void solve() {
+    
+}
+
+int main() {
+    // freopen("name.in", "r", stdin);
+    // freopen("name.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    preprocess();
+    int tt = 1;
+    // cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
+#endif
