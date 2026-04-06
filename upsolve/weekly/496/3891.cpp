5a953a1a0eb3debafd04c63bcfea11dafed2611e
ltn18 <nguyentamhoanglam@gmail.com>
2026-04-06 12:04:59 -0700

    upsolve 3891
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

---
+++ upsolve/weekly/496/3891.cpp
@@ -0,0 +1,120 @@
+// g++ -std=c++17 -DLOCAL template.cpp -o solution
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma region Debug
+#ifdef LOCAL
+template<typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T,U>& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << "[";
+    for (int i = 0; i < (int)v.size(); i++) os << (i ? ", " : "") << v[i];
+    return os << "]";
+}
+template<typename T>
+ostream& operator<<(ostream& os, const set<T>& s) {
+    os << "{";
+    int i = 0;
+    for (auto& x : s) os << (i++ ? ", " : "") << x;
+    return os << "}";
+}
+template<typename K, typename V>
+ostream& operator<<(ostream& os, const map<K,V>& m) {
+    os << "{";
+    int i = 0;
+    for (auto& [k, v] : m) os << (i++ ? ", " : "") << k << ": " << v;
+    return os << "}";
+}
+void _dbg() { cerr << endl; }
+template<typename T, typename... A>
+void _dbg(T t, A... a) { cerr << " " << t; if constexpr(sizeof...(a)) cerr << ","; _dbg(a...); }
+#define dbg(...) cerr << "\033[35m[" << #__VA_ARGS__ << "]\033[0m:", _dbg(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#pragma endregion
+
+using ll = long long;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using pii = pair<int, int>;
+
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFOR(i, n) for (int i = (n) - 1; i >= 0; i--)
+
+#define fi first
+#define se second
+#define pb push_back
+
+const int INF = 1e9;
+const ll LLINF = 2e18;
+
+const int MOD = 1e9+7;
+const int MOD_NTT = 998244353; // number theoretic transform (NTT)
+
+// 3891. Minimum Increase to Maximize Special Indices [Medium]
+class Solution {
+public:
+    ll minIncrease(vi& nums) {
+        int n = (int)nums.size();
+
+        if (n % 2) {
+            ll ans = 0;
+            for (int i = 1; i < n-1; i+=2) {
+                int cost = max(0, max(nums[i-1], nums[i+1]) + 1 - nums[i]);
+                ans += cost;
+            }
+            return ans;
+        }
+
+        int K = (n-2)/2;
+
+        vector<ll> pref(K+1, 0);
+        for (int i = 1; i < n-1; i+=2) {
+            int cost = max(0, max(nums[i-1], nums[i+1]) + 1 - nums[i]);
+            int idx = i/2+1;
+            pref[idx] = pref[idx-1] + cost;
+        }
+
+        vector<ll> suff(K+1, 0);
+        for (int i = n-2; i > 0; i-=2) {
+            int cost = max(0, max(nums[i-1], nums[i+1]) + 1 - nums[i]);
+            int idx = (n-i)/2;
+            suff[idx] = suff[idx-1] + cost;
+        }
+        
+        ll ans = LLINF;
+        for (int i = 0; i <= K; i++) {
+            ans = min(ans, pref[i] + suff[K-i]);
+        }
+        return ans;
+    }
+};
+
+#if !defined(CPTEST) && (defined(LOCAL) || defined(ONLINE_JUDGE))
+void preprocess() {
+    
+}
+
+// cout << Solution().solve() << '\n';
+void solve() {
+    
+}
+
+int main() {
+    // freopen("name.in", "r", stdin);
+    // freopen("name.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    preprocess();
+    int tt = 1;
+    // cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
+#endif
