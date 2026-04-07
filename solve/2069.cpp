4dab55ff0a27760ae5e91f918779aa1bf604a307
ltn18 <nguyentamhoanglam@gmail.com>
2026-04-06 21:32:12 -0700

    2069
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

---
+++ solve/2069.cpp
@@ -0,0 +1,120 @@
+// g++ -std=c++17 -DLOCAL template.cpp -o solution
+#include <bits/stdc++.h>
+using namespace std;
+
+#pragma region Debug
+#ifdef LOCAL
+template<typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T,U>& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+template<typename T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+    os << "[";
+    for (int i = 0; i < (int)v.size(); i++) os << (i ? ", " : "") << v[i];
+    return os << "]";
+}
+template<typename T>
+ostream& operator<<(ostream& os, const set<T>& s) {
+    os << "{";
+    int i = 0;
+    for (auto& x : s) os << (i++ ? ", " : "") << x;
+    return os << "}";
+}
+template<typename K, typename V>
+ostream& operator<<(ostream& os, const map<K,V>& m) {
+    os << "{";
+    int i = 0;
+    for (auto& [k, v] : m) os << (i++ ? ", " : "") << k << ": " << v;
+    return os << "}";
+}
+void _dbg() { cerr << endl; }
+template<typename T, typename... A>
+void _dbg(T t, A... a) { cerr << " " << t; if constexpr(sizeof...(a)) cerr << ","; _dbg(a...); }
+#define dbg(...) cerr << "\033[35m[" << #__VA_ARGS__ << "]\033[0m:", _dbg(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+#pragma endregion
+
+using ll = long long;
+using vi = vector<int>;
+using vii = vector<vector<int>>;
+using pii = pair<int, int>;
+
+#define REP(i, n) for (int i = 0; i < (n); i++)
+#define FOR(i, a, b) for (int i = (a); i <= (b); i++)
+#define FORD(i, a, b) for (int i = (a); i >= (b); i--)
+#define RFOR(i, n) for (int i = (n) - 1; i >= 0; i--)
+
+#define fi first
+#define se second
+#define pb push_back
+
+const int INF = 1e9;
+const ll LLINF = 2e18;
+
+const int MOD = 1e9+7;
+const int MOD_NTT = 998244353; // number theoretic transform (NTT)
+
+string directions[4] = {"East", "North", "West", "South"};
+
+// 2069. Walking Robot Simulation II [Medium]
+class Robot {
+public:
+    int W, H;
+    int P; // perimeter 
+    int rxy; // robot coordinate
+    bool moved;
+
+    Robot(int width, int height) {
+        W = width-1;
+        H = height-1;
+        P = 2 * W + 2 * H;
+        rxy = 0;
+        moved = false;
+    }
+    
+    void step(int num) {
+        moved = true;
+        rxy = (rxy + num) % P;
+    }
+    
+    vi getPos() {
+        if (rxy <= W) return {rxy, 0};
+        if (rxy <= W + H) return {W, rxy - W};
+        if (rxy <= 2 * W + H) return {2 * W + H - rxy, H};
+        return {0, P - rxy};
+    }
+    
+    string getDir() {
+        if (rxy == 0) return moved ? "South" : "East";
+        if (rxy <= W) return "East";
+        if (rxy <= W + H) return "North";
+        if (rxy <= 2 * W + H) return "West";
+        return "South";
+    }
+};
+
+#if !defined(CPTEST) && (defined(LOCAL) || defined(ONLINE_JUDGE))
+void preprocess() {
+    
+}
+
+// cout << Solution().solve() << '\n';
+void solve() {
+    
+}
+
+int main() {
+    // freopen("name.in", "r", stdin);
+    // freopen("name.out", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    preprocess();
+    int tt = 1;
+    // cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
+#endif
