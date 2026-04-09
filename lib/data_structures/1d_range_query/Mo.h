f9bf669019f6d80ab7d67747a79786efa13f51c3
ltn18 <nguyentamhoanglam@gmail.com>
2026-04-09 00:36:13 -0700

    Mo
    Signed-off-by: ltn18 <nguyentamhoanglam@gmail.com>

--- lib/data_structures/1d_range_query/Mo.h
+++ lib/data_structures/1d_range_query/Mo.h
@@ -28,7 +28,25 @@ struct Mo {
 
     // expand then shrink
     // add(++mo_R)/add(--mo_L) and remove(mo_R--)/remove(mo_L--)
+    inline void process_queries() {
+        // window size = mo_R - mo_L + 1 = 0 (empty)
+        int mo_L = 0, mo_R = -1;
+        for (int i = 0; i < Q.size(); i++) {
+            int L = Q[i].L, R = Q[i].R;
 
+            // expand
+            while (mo_L > L) add(--mo_L);
+            while (mo_R < R) add(++mo_R);
+
+            // shrink
+            while (mo_L < L) remove(mo_L--);
+            while (mo_R > R) remove(mo_R--);
+
+            // process answer here
+            // ans[Q[i].id] = ...
+        }
+    }
+    
     inline void add(int idx) {}
     inline void remove(int idx) {}
 };
